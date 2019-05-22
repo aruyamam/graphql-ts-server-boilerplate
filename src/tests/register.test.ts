3431a95b98844e6c0b60802dd1b53159cdfef0f0
aruyamam <aruyamam@gmail.com>
2019-05-22 16:55:02 +0900

    Setting up a Test Database with TypeORM

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,8 +1,12 @@
 import { request } from 'graphql-request';
-import { createConnection } from 'typeorm';
 
 import { host } from './constants';
 import { User } from '../entity/User';
+import { createTypeormConn } from '../utils/createTypeormConn';
+
+beforeAll(async () => {
+   await createTypeormConn();
+});
 
 const email = 'bob@bob.com';
 const password = 'dkfajfdakl';
@@ -17,7 +21,6 @@ test('Register user', async () => {
    const response = await request(host, mutation);
    expect(response).toEqual({ register: true });
 
-   await createConnection();
    const users = await User.find({ where: { email } });
    expect(users).toHaveLength(1);
 
