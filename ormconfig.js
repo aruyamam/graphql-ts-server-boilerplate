2c24b7e5213422975a201cc5bd852834f2fb78a9
aruyamam <aruyamam@gmail.com>
2019-05-22 12:55:47 +0900

    Setting up Jest

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,18 @@
 module.exports = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: ['src/entity/**/*.ts'],
-    migrations: ['src/migration/**/*.ts'],
-    subscribers: ['src/subscriber/**/*.ts'],
-    cli: {
-        entitiesDir: 'src/entity',
-        migrationsDir: 'src/migration',
-        subscribersDir: 'src/subscriber'
-    }
+   type: 'postgres',
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT,
+   username: process.env.DB_USERNAME,
+   password: process.env.DB_PASS,
+   database: process.env.DB_NAME,
+   synchronize: true,
+   logging: true,
+   entities: ['src/entity/**/*.ts'],
+   migrations: ['src/migration/**/*.ts'],
+   subscribers: ['src/subscriber/**/*.ts'],
+   cli: {
+      entitiesDir: 'src/entity',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber'
+   }
 };
